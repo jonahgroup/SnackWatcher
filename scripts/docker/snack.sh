9fbcafdee50cdafc0b327c458b90028f167010e1
primitiveuniverse <markb@anaker.com>
2016-05-17 17:57:33 -0400

    Changed the git clone command to only get the HEAD revision and not the entire history

--- scripts/docker/snack.sh
+++ scripts/docker/snack.sh
@@ -4,7 +4,7 @@ echo [SNACK]
 echo [Remove any existing clones]
 rm -rf SnackWatcher
 echo [Clone a fresh copy of SnackWatcher]
-git clone https://github.com/jonahgroup/SnackWatcher.git
+git clone --depth 1 https://github.com/jonahgroup/SnackWatcher.git
 echo [Run SnackWatcher]
 echo [Note: If this fails due to a Docker related issue, try running "sh docker_build.sh" and execute this script again]
 sh SnackWatcher/scripts/docker/docker_run.sh | sed "s/^/\t/"
